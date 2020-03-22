a4916494a4fa6852b966bffa1595d8c00d173def
MaxLyman <42448211+MaxLyman@users.noreply.github.com>
2020-03-21 22:12:47 -0400

    writing to csv. syntax.
    added writing to a csv file that is currently just stored in the working directory, updated some syntax

--- src/main/java/Phish_Reader.java
+++ src/main/java/Phish_Reader.java
@@ -11,44 +11,45 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Phish_Reader {
-    private final String file;
     private static final String CSV_FILE_PATH = "./result.csv";
+    private final String file;
     private JSONParser parser = new JSONParser();
 
     public Phish_Reader(String fileName) {
         this.file = fileName;
+        writeToFile();
     }
 
-    private void writeToFile(){
+    private void writeToFile() {
         File outFile = new File(CSV_FILE_PATH);
         try {
             //creating file
             FileWriter output = new FileWriter(outFile);
             //creating csv with , as separators
-            CSVWriter writer = new CSVWriter(output, ',',
-                CSVWriter.NO_QUOTE_CHARACTER, CSVWriter.DEFAULT_ESCAPE_CHARACTER,
-                CSVWriter.DEFAULT_LINE_END);
+            CSVWriter writer = new CSVWriter(output, ',', CSVWriter.NO_QUOTE_CHARACTER,
+                CSVWriter.DEFAULT_ESCAPE_CHARACTER, CSVWriter.DEFAULT_LINE_END);
             List<String[]> data = new ArrayList<>();
 
             //getting JSON object elements
             Object obj = parser.parse(new FileReader(file));
             JSONArray array = (JSONArray) obj;
-            for(int i = 0; i < array.size(); i++){
+            for (int i = 0; i < array.size(); i++) {
                 JSONObject jsonObj = (JSONObject) array.get(i);
+                //package
                 String name = jsonObj.get("name").toString();
                 String lastRun = jsonObj.get("last_run").toString();
                 String last_PPP = jsonObj.get("last_phish_prone_percentage").toString();
 
-                String[] elementData = new String[]{name,lastRun,last_PPP};
+                String[] elementData = new String[] {name, lastRun, last_PPP};
                 data.add(elementData);
             }
 
             writer.writeAll(data);
             writer.close();
 
-        } catch (FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
